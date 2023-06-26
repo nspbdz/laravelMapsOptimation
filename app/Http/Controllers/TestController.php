744862073a31d283d1c7df927b354aad5283db43
Mohamad Alfian Ramadhan <56055855+nspbdz@users.noreply.github.com>
2023-06-26 22:00:18 +0700

    fix: data-endLocation

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,123 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lokasi;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function index()
     {
+        $endLocation = Lokasi::get()->toArray();
 
-        // $startLat = -6.175392; // Latitude titik awal
-        // $startLng = 106.827153; // Longitude titik awal
         $startLocations = [
             [
                 'name' => 'parung panjang',
-                'latitude' => -6.368107,
-                'longitude' => 106.553387
+                'lat' => -6.368107,
+                'lng' => 106.553387
             ],
 
             [
                 'name' => 'Indramayu',
-                'latitude' => -6.327583,
-                'longitude' => 108.324936
+                'lat' => -6.327583,
+                'lng' => 108.324936
             ],
 
 
             [
                 'name' => 'Jakarta Location 1',
-                'latitude' => -6.200,
-                'longitude' => 106.700,
+                'lat' => -6.200,
+                'lng' => 106.700,
             ],
         ];
 
-        // dd(count($startLocations));
-
-
-        $endLocation = [
-            [
-                'name' => 'tegal parang',
-                'latitude' => -6.249095,
-                'longitude' => 106.828162
-            ],
-            [
-                'name' => 'Jakarta Location 1',
-                'latitude' => -6.200,
-                'longitude' => 106.700,
-            ],
-            [
-                'name' => 'Jakarta Location 2',
-                'latitude' => -6.201,
-                'longitude' => 106.701,
-            ],
-            [
-                'name' => 'Jakarta Location 3',
-                'latitude' => -6.202,
-                'longitude' => 106.702,
-            ],
-            [
-                'name' => 'Jakarta Location 4',
-                'latitude' => -6.203,
-                'longitude' => 106.703,
-            ],
-            [
-                'name' => 'Jakarta Location 5',
-                'latitude' => -6.204,
-                'longitude' => 106.704,
-            ],
-            [
-                'name' => 'Jakarta Location 6',
-                'latitude' => -6.205,
-                'longitude' => 106.705,
-            ],
-            [
-                'name' => 'Jakarta Location 7',
-                'latitude' => -6.206,
-                'longitude' => 106.706,
-            ],
-            [
-                'name' => 'Jakarta Location 8',
-                'latitude' => -6.207,
-                'longitude' => 106.707,
-            ],
-            [
-                'name' => 'Jakarta Location 9',
-                'latitude' => -6.208,
-                'longitude' => 106.708,
-            ],
-            [
-                'name' => 'Jakarta Location 10',
-                'latitude' => -6.209,
-                'longitude' => 106.709,
-            ],
-            [
-                'name' => 'Jakarta Location 11',
-                'latitude' => -6.210,
-                'longitude' => 106.710,
-            ],
-            [
-                'name' => 'Jakarta Location 12',
-                'latitude' => -6.211,
-                'longitude' => 106.711,
-            ],
-            [
-                'name' => 'Jakarta Location 13',
-                'latitude' => -6.212,
-                'longitude' => 106.712,
-            ],
-            [
-                'name' => 'Jakarta Location 14',
-                'latitude' => -6.213,
-                'longitude' => 106.713,
-            ],
-            [
-                'name' => 'Jakarta Location 15',
-                'latitude' => -6.214,
-                'longitude' => 106.714,
-            ],
-
-
-        ];
 
         $totalEndLocations = count($endLocation); // Jumlah total lokasi yang akan dibagi
         $totalDrivers = count($startLocations); // Jumlah total driver
@@ -149,7 +62,7 @@ class TestController extends Controller
             $data['driver' . $i] = $startLocations[$i];
 
             for ($j = 0; $j < $outputArray[$i]['itemCount']; $j++) {
-                $data['driver' . $i][$j] = $this->getNearestLocation($startLocations[$i]['latitude'], $startLocations[$i]['longitude'], $endLocation);
+                $data['driver' . $i][$j] = $this->getNearestLocation($startLocations[$i]['lat'], $startLocations[$i]['lng'], $endLocation);
 
                 $key = array_search($data['driver' . $i][$j], $endLocation);
                 if ($key !== false) {
@@ -170,8 +83,8 @@ class TestController extends Controller
         $nearestDistance = null;
 
         foreach ($locations as $location) {
-            $lat = $location['latitude'];
-            $lng = $location['longitude'];
+            $lat = $location['lat'];
+            $lng = $location['lng'];
 
             $distance = $this->haversineDistance($startLat, $startLng, $lat, $lng);
 
